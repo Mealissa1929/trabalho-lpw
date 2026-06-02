86fca598d840a4c982808a3df019ada086a937bb
Melissa Silveira <melissas.ifpr2024@gmail.com>
2026-06-02 18:47:54 -0300

    Update listagem.php

--- listagem.php
+++ listagem.php
@@ -1,44 +1,66 @@
+<?php
+
+require_once("dao/CopaDAO.php");
+
+$dao = new CopaDAO();
+$copas = $dao->listar();
+
+?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Copas do Mundo</title>
+    <title>Listagem de Copas</title>
 </head>
 <body>
 
-    <h1>Cadastro de Copas do Mundo</h1>
+<h1>Copas Cadastradas</h1>
 
-    <h3>Listagem</h3>
+<a href="index.php">Nova Copa</a>
 
-    <table border="1">
-        <!-- Cabeçalho -->
-        <tr>
-            <th>ID</th>
-            <th>Ano</th>
-            <th>Sede</th>
-            <th>Campeão</th>
-            <th>Confederação Sede</th>
-            <th>Quantidade de Seleções</th>
-            <th>Imagem</th>
-        </tr> 
+<table border="1">
 
-        <!-- Dados -->
-        <?php foreach($copas as $c): ?>
-            <tr>
-                <td><?= $c["id"] ?></td>
-                <td><?= $c["Ano"] ?></td>
-                <td>
-                    <?= ?>
-                </td>
-                <td><?= $c["qtd_paginas"] ?></td>
-                <td><?= $c["autor"] ?></td>
-                <td><a href="livros_excluir.php?id=<?=$l['id']?>"
-                onclick="if(!confirm('Confirma a exclusão?')) return false;"
-                >Excluir</a></td>
-            </tr>
-        
-        <?php endforeach; ?>
-    </table>
-    
+    <tr>
+        <th>ID</th>
+        <th>Ano</th>
+        <th>Sede</th>
+        <th>Campeão</th>
+        <th>Confederação</th>
+        <th>Quantidade</th>
+        <th>Imagem</th>
+        <th>Ações</th>
+    </tr>
+
+    <?php foreach($copas as $c): ?>
+
+    <tr>
+        <td><?= $c["id"] ?></td>
+        <td><?= $c["ano"] ?></td>
+        <td><?= $c["sede"] ?></td>
+        <td><?= $c["campeao"] ?></td>
+        <td><?= $c["confederacao"] ?></td>
+        <td><?= $c["quantidade"] ?></td>
+
+        <td>
+            <img src="<?= $c["imagem"] ?>" width="100">
+        </td>
+
+        <td>
+            <a href="copas_excluir.php?id=<?= $c['id'] ?>"
+               onclick="return confirm('Confirma a exclusão?')">
+               Excluir
+            </a>
+        </td>
+    </tr>
+
+    <?php endforeach; ?>
+
+</table>
+
+<br>
+
+<a href="cards.php">Visualizar em Cards</a>
+
+</body>
+</html>
