0049bd8be0629ef8ec4c1e1c7f5bfafd707f776f
Melissa Silveira <melissas.ifpr2024@gmail.com>
2026-06-02 18:45:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -45,50 +45,7 @@ if(isset($_POST['ano'])) {
 
 
 
-?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Copas do Mundo</title>
-</head>
-<body>
 
-    <h1>Cadastro de Copas do Mundo</h1>
-
-    <h3>Listagem</h3>
-
-    <table border="1">
-        <!-- Cabeçalho -->
-        <tr>
-            <th>ID</th>
-            <th>Ano</th>
-            <th>Sede</th>
-            <th>Campeão</th>
-            <th>Confederação Sede</th>
-            <th>Quantidade de Seleções</th>
-            <th>Imagem</th>
-        </tr> 
-
-        <!-- Dados -->
-        <?php foreach($copas as $c): ?>
-            <tr>
-                <td><?= $c["id"] ?></td>
-                <td><?= $c["Ano"] ?></td>
-                <td>
-                    <?= ?>
-                </td>
-                <td><?= $c["qtd_paginas"] ?></td>
-                <td><?= $c["autor"] ?></td>
-                <td><a href="livros_excluir.php?id=<?=$l['id']?>"
-                onclick="if(!confirm('Confirma a exclusão?')) return false;"
-                >Excluir</a></td>
-            </tr>
-        
-        <?php endforeach; ?>
-    </table>
-    
 
 
     <h3>Formulário</h3>
